f73a0fc9f789435ba53558951a1e03f219cc7546
hillary <73066202@buaa.edu.cn>
2026-04-21 20:14:13 +0800

    chore: 配置前端依赖

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,16 @@
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import { createPinia } from 'pinia'
+import router from './router'
 import './style.css'
+import './styles/global.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(ElementPlus)
+app.use(createPinia())
+app.use(router)  // 注册路由
+
+app.mount('#app')
---
+++ src/router/index.ts
@@ -0,0 +1,8 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [] // 后续添加
+})
+
+export default router
